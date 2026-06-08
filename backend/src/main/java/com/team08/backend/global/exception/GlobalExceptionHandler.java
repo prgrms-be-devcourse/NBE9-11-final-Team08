fc457f021b9b4c72ce59271b20dad10f30b9eb99
eunhye <gp79810@gmail.com>
2026-06-08 17:12:27 +0900

    refactor: 예외 핸들러

---
+++ backend/src/main/java/com/team08/backend/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.team08.backend.global.exception;
+
+import com.team08.backend.global.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ApiResponse.error(errorCode));
+    }
+
+}
