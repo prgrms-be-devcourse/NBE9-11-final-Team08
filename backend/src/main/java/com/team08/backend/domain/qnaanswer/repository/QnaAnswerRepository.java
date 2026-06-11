d87ac6afbf3a99e4b0782eb78622659b1485ea14
eunhye <gp79810@gmail.com>
2026-06-11 10:33:05 +0900

    chore:question/answer -> lectureQna으로 묶음
    - 의존관계 명확화

--- backend/src/main/java/com/team08/backend/domain/qnaanswer/repository/QnaAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team08.backend.domain.qnaanswer.repository;
-
-import com.team08.backend.domain.qnaanswer.entity.QnaAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QnaAnswerRepository extends JpaRepository<QnaAnswer, Long> {
-}
