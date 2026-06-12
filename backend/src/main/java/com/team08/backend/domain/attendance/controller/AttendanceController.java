a0eef6bb85c6e4f3dcfc99b3a61fdb626f71c814
kwon15477 <kwon15477@gmail.com>
2026-06-12 10:52:37 +0900

    refactor: 출석체크 userId를 토큰에서 가져오도록 수정

--- backend/src/main/java/com/team08/backend/domain/attendance/controller/AttendanceController.java
+++ backend/src/main/java/com/team08/backend/domain/attendance/controller/AttendanceController.java
@@ -2,14 +2,15 @@ package com.team08.backend.domain.attendance.controller;
 
 import com.team08.backend.domain.attendance.dto.AttendanceResponse;
 import com.team08.backend.domain.attendance.service.AttendanceService;
+import com.team08.backend.global.auth.principal.LoginUserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,16 +24,16 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
-    // [사용자] 출석 체크 /api/attendances
+    // [사용자] 출석 체크 /api/attendance정
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "출석 체크", description = "당일 출석을 수행하고 연속/누적 출석일수를 반환합니다.")
     public AttendanceResponse checkIn(
             @Parameter(description = "출석을 수행할 사용자의 ID", example = "1")
-            @RequestParam Long userId) {
+            @AuthenticationPrincipal LoginUserPrincipal loginUserPrincipal) {
         LocalDate today = LocalDate.now();
 
         // [사용자] 출석체크
-        return attendanceService.checkIn(userId, today);
+        return attendanceService.checkIn(loginUserPrincipal.user().id(), today);
     }
 }
