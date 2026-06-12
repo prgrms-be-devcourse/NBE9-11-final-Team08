b1ba77dbed1b37fb5fccecf954e3e25a6c47d39f
eunhye <gp79810@gmail.com>
2026-06-12 16:24:21 +0900

    feat: 챕터 속 강의 조회 메소드 정의(repository)

--- backend/src/main/java/com/team08/backend/domain/lecture/repository/LectureRepository.java
+++ backend/src/main/java/com/team08/backend/domain/lecture/repository/LectureRepository.java
@@ -3,5 +3,14 @@ package com.team08.backend.domain.lecture.repository;
 import com.team08.backend.domain.lecture.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+    // 챕터의 강의를 순서대로 조회
+    List<Lecture> findByChapterIdOrderByOrderNoAsc(Long chapterId);
+
+    // 챕터의 첫 번째 강의 조회
+    Optional<Lecture> findFirstByChapterIdOrderByOrderNoAsc(Long chapterId);
 }
