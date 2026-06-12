80e6d0129a4e706f5983c7d166e4bbd84bd23d6f
kwon15477 <kwon15477@gmail.com>
2026-06-12 09:38:58 +0900

    chore: ResponseStatus 추가

--- backend/src/main/java/com/team08/backend/domain/attendance/controller/AttendanceController.java
+++ backend/src/main/java/com/team08/backend/domain/attendance/controller/AttendanceController.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -24,6 +26,7 @@ public class AttendanceController {
 
     // [사용자] 출석 체크 /api/attendances
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "출석 체크", description = "당일 출석을 수행하고 연속/누적 출석일수를 반환합니다.")
     public AttendanceResponse checkIn(
             @Parameter(description = "출석을 수행할 사용자의 ID", example = "1")
