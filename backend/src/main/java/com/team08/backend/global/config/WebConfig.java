91da51a5b53bb47614ea38a5f822bd174b1df35e
kwon15477 <kwon15477@gmail.com>
2026-06-18 22:28:33 +0900

    feat: WebConfig

---
+++ backend/src/main/java/com/team08/backend/global/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.team08.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// TODO 제대로 된 건지 모르겠음... 일단 돌아감
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // 프론트엔드 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
+                .allowCredentials(true); // 쿠키/인증 정보 허용
+    }
+}
