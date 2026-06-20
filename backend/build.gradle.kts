6343bfebb57ec68c49588fb6ef809760a5c09dd0
kwon15477 <kwon15477@gmail.com>
2026-06-20 20:47:06 +0900

    build: Redis 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
