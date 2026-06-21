bb1a7576d1ea4f494a57c1758fd35741ba0ae26d
kwon15477 <kwon15477@gmail.com>
2026-06-21 21:11:05 +0900

    fix: 프론트 쿠폰 조기 마감 버그 수정

--- backend/src/main/java/com/team08/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/team08/backend/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOriginPatterns("*") // 모든 프론트엔드 도메인/IP 허용 (로컬/폰 접속 테스트용)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
                 .allowCredentials(true); // 쿠키/인증 정보 허용
     }
 }
