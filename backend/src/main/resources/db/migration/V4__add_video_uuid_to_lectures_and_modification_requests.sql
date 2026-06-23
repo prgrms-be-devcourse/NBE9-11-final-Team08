051752ab157980cd204a0d317f9147dd0992972b
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-06-23 14:49:48 +0900

    chore: Lectures 및 수정요청 테이블 videoUuid 컬럼 추가를 위한 Flyway V4 스크립트 작성 (#237)

--- backend/src/main/resources/db/migration/V4__add_video_uuid_to_lectures_and_modification_requests.sql
+++ backend/src/main/resources/db/migration/V4__add_video_uuid_to_lectures_and_modification_requests.sql
@@ -1,2 +1,5 @@
-    ALTER TABLE lectures ADD COLUMN video_uuid VARCHAR(36);
-    ALTER TABLE lecture_modification_requests ADD COLUMN after_video_uuid VARCHAR(36);
+ALTER TABLE lectures ADD COLUMN video_uuid VARCHAR(36);
+ALTER TABLE lecture_modification_requests ADD COLUMN after_video_uuid VARCHAR(36);
+
+CREATE INDEX idx_lectures_video_uuid ON lectures(video_uuid);
+CREATE INDEX idx_lecture_mod_requests_after_video_uuid ON lecture_modification_requests(after_video_uuid);
